bb5acfd0c9dbc9eccf7829678b717814fb6d9243
NickNickNick <thai09@yandex.ru>
2012-02-28 11:55:22 +0700

    Update homework1.4/task.py

--- homework1.4/task.py
+++ homework1.4/task.py
@@ -26,6 +26,55 @@ def calculate():
     #INCORRECT
   
     p = []
+   
+for i in range(len(colors)):
+    p.append([1./(len(colors)*len(colors[i]))] * len(colors[i]))
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        s=[]
+        for j in range(len(p[i])):
+            hit = (Z == colors[i][j])
+            s.append(p[i][j] * (hit * sensor_right + (1-hit) *(1-sensor_right)))
+        q.append(s)
+    zz=0
+    for i in range(len(p)):
+        for j in range(len(p[i])):
+            zz =zz+q[i][j]
+    #print "Z",zz,"ненормализ",q
+    for i in range(len(q)):
+        for j in range(len(p[i])):
+            q[i][j]=q[i][j]/zz
+    #print "нормализ.",q
+    return q
+
+
+def move(p, U):
+    q = []
+    qq=p
+    for i in range(len(p)):
+        ss=[]
+        for j in range(len(p[i])):
+            s = p_move * p[i][(j-U[0]) % len(p[i])]
+            s = s + (1-p_move) * p[i][(j-U[0]+1) % len(p[i])]
+            ss.append(s)
+        q.append(ss)
+    print "Q",q
+    for j in range(len(p[i])):
+        for i in range(len(p)):
+            s = p_move * q[(i-U[1]) % len(p)][j]
+            s = s + (1-p_move) * q[(i-U[0]+1) % len(p)][j]
+            qq[i][j]=s
+    print "QQ",qq
+    return qq
+
+for k in range(len(measurements)):
+    p = sense(p, measurements[k])
+    p = move(p, motions[k])
+
+
+
 
     #Your probability array must be printed 
     #with the following code.
